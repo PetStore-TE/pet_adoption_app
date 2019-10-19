29a98a5cb2d65f1299099fdcd27adf2b3b852c5b
Tom Phillips <tomphillipsmusic@gmail.com>
2019-10-19 16:05:15 -0400

    encountered rendering issue

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
   const [breeds, updateBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+  useEffect(() => {
+    updateBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
   return (
     <div className="search-params">
       <form>
